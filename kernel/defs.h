d348827e534d26652deb23c266c849c6d726a073
zzy0304Raymond <2205016658@qq.com>
2024-08-16 15:21:31 +0800

    lab5

--- kernel/defs.h
+++ kernel/defs.h
@@ -170,7 +170,8 @@ uint64          walkaddr(pagetable_t, uint64);
 int             copyout(pagetable_t, uint64, char *, uint64);
 int             copyin(pagetable_t, char *, uint64, uint64);
 int             copyinstr(pagetable_t, char *, uint64, uint64);
-
+int             iscowfault(pagetable_t,uint64);
+int             cowalloc(pagetable_t,uint64);
 // plic.c
 void            plicinit(void);
 void            plicinithart(void);
--- kernel/trap.c
+++ kernel/trap.c
@@ -65,7 +65,23 @@ usertrap(void)
     intr_on();
 
     syscall();
-  } else if((which_dev = devintr()) != 0){
+  }
+  else if (r_scause() == 15)
+  {
+    uint64 va = r_stval(); //得到虚拟地址
+    if(va>=p->sz){
+      p->killed = 1; // 若越界，则杀死进程
+    }
+    if (iscowfault(p->pagetable, va))
+    { 
+      if (cowalloc(p->pagetable, va) == 0)
+      {
+        p->killed = 1; 
+      }
+    }
+  }
+  else if ((which_dev = devintr()) != 0)
+  {
     // ok
   } else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
